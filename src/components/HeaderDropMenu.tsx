c9f25890eec9a01a73347955a58ea12f9277e794
Louis <louislee0996@gmail.com>
2024-04-17 19:50:31 -0400

    feat:signup&signinForm

--- src/components/HeaderDropMenu.tsx
+++ src/components/HeaderDropMenu.tsx
@@ -1,44 +1,68 @@
+import React, { useState } from "react";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import SignUpForm from "./SignUpDialog";
+import SignInForm from "./SignInDialog";
+import { AlignJustify } from "lucide-react";
 
 export default function HeaderDropDownMenu() {
-    return (
-            <div className="flex items-center border border-solid border-gray-400 rounded-full hover:border-gray-600 py-1 px-2">
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                        <div className="flex items-center">
-                            <div className="px-1">
-                                <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 32 32"
-                                aria-hidden="true"
-                                role="presentation"
-                                focusable="false"
-                                className="block fill-none h-4 w-4 stroke-current overflow-visible"
-                                >
-                                <g fill="none">
-                                    <path d="M2 16h28M2 24h28M2 8h28"></path>
-                                </g>
-                            </svg>
-                            </div>
-                            <div className="relative w-9 h-9 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                            <svg className="absolute w-11 h-11 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
-                        </div>
-                        </div>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuItem>Sign Up</DropdownMenuItem>
-                            <DropdownMenuItem>Log In</DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Gift Cards</DropdownMenuItem>
-                            <DropdownMenuItem>Help Center</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-    )
+  const [openSignUpDialog, setOpenSignUpDialog] = useState(false);
+  const handleSignUpClick = () => {
+    setOpenSignUpDialog(true);
+  };
+  const handleCloseSignUpDialog = () => {
+    setOpenSignUpDialog(false);
+  };
+  const [openSignInDialog, setOpenSignInDialog] = useState(false);
+  const handleSignInClick = () => {
+    setOpenSignInDialog(true);
+  };
+  const handleCloseSignInDialog = () => {
+    setOpenSignInDialog(false);
+  };
+  return (
+    <div className="flex items-center border border-solid border-gray-400 rounded-full hover:border-gray-600 py-0.5 px-2">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="flex items-center">
+            <div className="px-1">
+              <AlignJustify />
+            </div>
+            <div className="relative w-9 h-9 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+              <svg
+                className="absolute w-11 h-11 text-gray-400 -left-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={handleSignUpClick}>
+            Sign Up
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleSignInClick}>
+            Sign In
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Gift Cards</DropdownMenuItem>
+          <DropdownMenuItem>Help Center</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <SignUpForm open={openSignUpDialog} onClose={handleCloseSignUpDialog} />
+      <SignInForm open={openSignInDialog} onClose={handleCloseSignInDialog} />
+    </div>
+  );
 }
