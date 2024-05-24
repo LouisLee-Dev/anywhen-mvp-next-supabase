b8e014c804e9d8cb88305da0f4dbf85f0afd8487
Louis <louislee0996@gmail.com>
2024-05-24 14:26:38 +0000

    feat: add trips page

---
+++ src/features/tips/section/TripsSection.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { CirclePlus, Plus } from "lucide-react";
+import { useState } from "react";
+
+export default function PropertiesSection() {
+  return (
+    <div className="w-full">
+      <h1>Trips</h1>
+    </div>
+  );
+}
