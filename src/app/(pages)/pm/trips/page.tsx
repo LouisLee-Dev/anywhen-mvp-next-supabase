51e3e94fb90dc9af4278ae5b70134541abd9039a
Louis <louislee0996@gmail.com>
2024-05-24 14:41:55 +0000

    feat: add trips page

--- src/app/(pages)/pm/trips/page.tsx
+++ src/app/(pages)/pm/trips/page.tsx
@@ -1,10 +1,10 @@
-import TripsSection from "@features/trips/sections/TripsSection";
+import TripsSection from "@/features/trips/sections/TripsSection";
 
 export default function Trips() {
   return (
     <div className="page-content-wrapper">
       <div className="lg:px-[8rem]">
-        <h1>Trips</h1>
+        <TripsSection />
       </div>
     </div>
   );
